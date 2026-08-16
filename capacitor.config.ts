657df2b0bc61f0e863cc67d501d02abc89ea919b
eldemogorgon2019-prog <eldemogorgon2019@gmail.com>
2026-08-16 18:24:14 +0200

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marvel.ordencronologico',
+  appName: 'Marvel Orden Cronológico',
+  webDir: 'www'
+};
+
+export default config;
